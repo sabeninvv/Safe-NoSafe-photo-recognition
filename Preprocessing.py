25ba37417a7b65b80cbcd611112d318617f38a80
sabeninvv <44149415+sabeninvv@users.noreply.github.com>
2020-03-11 12:36:03 +0700

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -14,6 +14,7 @@ import pandas as pd
 from PIL import Image
 from tensorflow.compat.v2.nn import relu6
 from tensorflow.keras.models import load_model, model_from_json
+from tensorflow.keras import utils
 from sklearn.model_selection import train_test_split
 
 
@@ -65,58 +66,34 @@ def crop2img2arr(path, in_shape=(128, 128), crop=False, center=False, scale=True
     return imgarr
 
 
-def getConfMatrix(model, x, y, to_categorical=False):
+def getConfMatrix(model, X_Test, y_true, to_categorical=False, y_pred=None):
     '''
-    Создание матрицы ошибок
+    Создание матрицы ошибок (в %)
     :param model: обученная tf.keras модель
-    :param x: data to predict
-    :param y: true labels
-    #y_Predicted: predict labels
+    :param X_Test: data to predict
+    :param y_true: true labels
+    :param y_pred: predict labels
     :param to_categorical: если labels в one hot encoding
     '''
     if to_categorical:
-        num_pics_test = np.unique(y, return_counts=True, axis=-2)[1]
+        num_pics_test = np.unique(y_true, return_counts=True, axis=-2)[1]
     else:
-        num_pics_test = np.unique(y, return_counts=True)[1]
-    data = {'y_Actual': y,
-            'y_Predicted': [p.argmax() for p in model.predict(x)]
-            }
+        num_pics_test = np.unique(y_true, return_counts=True)[1]
+    if y_pred:
+        data = {'y_Actual': y_true,
+                'y_Predicted': y_pred
+                }
+    else:
+        data = {'y_Actual': y_true,
+                'y_Predicted': [p.argmax() for p in model.predict(X_Test)]
+                }
     df = pd.DataFrame(data, columns=['y_Actual', 'y_Predicted'])
     confusion_matrix = pd.crosstab(df['y_Actual'],
                                    df['y_Predicted'],
                                    rownames=['Actual'],
                                    colnames=['Predicted'],
                                    margins=True)
-    print('====>')
-    for row, countInClass in enumerate(num_pics_test):
-        confusion_matrix.loc[row] = (confusion_matrix.loc[row] * 100) / countInClass
-        confusion_matrix.loc[row] = np.around(confusion_matrix.loc[row], 3)
-    return confusion_matrix
-
-
-def conf_matrix_fitgen(model, test_generator, batch_size):
-    Y_pred = model.predict_generator(test_generator, test_generator.samples // batch_size + 1)
-    y_pred = np.argmax(Y_pred, axis=1)
-    flag0, flag1, flag2 = (0, 0, 0)
-    for i in test_generator.labels.astype('int32'):
-        if i == 0:
-            flag0 += 1
-        elif i == 1:
-            flag1 += 1
-        else:
-            flag2 += 1
-    num_pics_test = [flag0, flag1, flag2]
-
-    data = {'y_Actual': test_generator.labels.astype('int32'),
-            'y_Predicted': y_pred
-            }
-    df = pd.DataFrame(data, columns=['y_Actual', 'y_Predicted'])
-    confusion_matrix = pd.crosstab(df['y_Actual'],
-                                   df['y_Predicted'],
-                                   rownames=['Actual'],
-                                   colnames=['Predicted'],
-                                   margins=True)
-    print('====>')
+    print('======>')
     for row, countInClass in enumerate(num_pics_test):
         confusion_matrix.loc[row] = (confusion_matrix.loc[row] * 100) / countInClass
         confusion_matrix.loc[row] = np.around(confusion_matrix.loc[row], 3)
@@ -150,9 +127,9 @@ def collaps_fich_matrix(path, model_for_vis, in_shape=(128, 128)):
             channel_image += 128
             channel_image = np.clip(channel_image, 0, 255).astype('uint8')
             display_grid[col * size: (col + 1) * size,
-                        row * size: (row + 1) * size
-                        ] = (display_grid[col * size: (col + 1) * size,
-                             row * size: (row + 1) * size] + channel_image) / 2
+            row * size: (row + 1) * size
+            ] = (display_grid[col * size: (col + 1) * size,
+                 row * size: (row + 1) * size] + channel_image) / 2
     return display_grid
 
 
@@ -202,18 +179,19 @@ def compare_images2(model_for_vis1, model_for_vis2, path, t, in_shape=(128, 128)
     plt.axis("off")
 
 
-def new_smooth_label(y_to_categorical, smooth_factor1=0.2, smooth_factor2=0.1):
+def new_smooth_label(categorical_labels, smooth_factor=0.2):
+    '''
+    Функция "распыляет" метки,
+    переданные в формате onehotencoding
+    :param categorical_labels: метки в формате onehotencoding
+    :param smooth_factor: процент "размытия"
+    '''
     new_y = []
-    for y_ in y_to_categorical:
-        if np.argmax(y_) == 2:
-            y = y_ * (1 - smooth_factor1)
-            y += smooth_factor1 / 3
-            new_y.append(y)
-        else:
-            y = y_ * (1 - smooth_factor2)
-            y += smooth_factor2 / 3
-            new_y.append(y)
-    new_y = np.array(new_y)
+    for y_ in categorical_labels:
+        y = y_ * (1 - smooth_factor)
+        y += (smooth_factor / categorical_labels.shape[-1])
+        new_y.append(y)
+    new_y = np.array(new_y, dtype=np.float16)
     return new_y
 
 
@@ -334,6 +312,7 @@ def loadH5(path, rescale=False):
     :param path: путь к файлу
     :param rescale: нужно ли нормировать до [-1 1]
     '''
+
     def helpScale(h5py_File, name):
         x = h5py_File[name][:].astype(np.float32)
         x /= 128.
@@ -353,3 +332,161 @@ def loadH5(path, rescale=False):
         y_val = f['y_val'][:]
         y_test = f['y_test'][:]
     return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+def gen2pred(model, path, x, y, batch=1000):
+    '''
+    Генератор. Проходит по файлу, нормализует батч х,
+    предсказывает через модель.
+    Выдаёт предсказанные y и истинные y.
+    :param model: Модель в Keras
+    :param path: Путь к .hdf5 файлу
+    :param x: str. Ключ к X_train
+    :param y: str. Ключ к y_train
+    :param batch: Размер батча
+    '''
+    inx = 0 # Индекс для контроля перемещения по .hdf5
+    y_pred_all = [] # Список с распознанными метками классов
+    y_true_all = [] # Список с истинными метками классов
+    if batch == 0:
+        batch = 1
+    while True:
+        with h5py.File(path, 'r') as f:
+            x_ = f[x][inx:inx + batch]
+            x_ = x_.astype(np.float32)
+            x_ /= 128.
+            x_ -= 1.
+
+            y_pred = model.predict(x_)
+            y_pred = [p.argmax() for p in y_pred]  # -> list
+            y_true = f[y][inx:inx + batch]
+            y_true = y_true.astype(np.uint8)  # -> np.uint8
+            y_pred_all.extend(y_pred)  # -> list
+            y_true_all.extend(y_true)  # -> list
+            yield y_pred_all, y_true_all
+
+            # Проверка. Если прошли весь файл, создаём исключение StopIteration
+            if inx + batch == f[x].shape[0]:
+                return False
+            # Проверка. Если через следующую итерацию inx выбросит за пределы файла
+            elif inx + 2 * batch > f[x].shape[0]:
+                # Выполняем сдвиг inx
+                inx += batch
+                # Изменяем batch, чтобы не выпрыгнуть за пределы файла
+                batch = f[x].shape[0] - inx
+            else:
+                inx += batch
+
+
+def getConfUseGen(model, path, x, y, batch=1000):
+    '''
+    Создёт матрицу ошибок, используя генератор.
+    :param model: Модель в Keras
+    :param path: Путь к .hdf5 файлу
+    :param x: str. Ключ к X_train
+    :param y: str. Ключ к y_train
+    :param batch: Размер батча
+    '''
+    y_pred, y_true = (0., 0.)
+    gen = gen2pred(model, path, x, y, batch=batch)
+    while True:
+        try:
+            y_pred, y_true = next(gen)
+        except StopIteration:
+            break
+    confMtrx = getConfMatrix(model=model,
+                             X_Test=None,
+                             y_true=y_true,
+                             to_categorical=False,
+                             y_pred=y_pred)
+    return confMtrx
+
+
+def genFromH5(path, x, y, batch, num_classes, to_categorical=False):
+    '''
+    Генератор. Возвращает сбалансированный батч (x, y)
+    x - нормализуется. x/=128. ; x-=1.
+    :param path: Путь до .hdf5 файла
+    :param x: str. Ключ к X_train
+    :param y: str. Ключ к y_train
+    :param batch: Размер батча
+    :param num_classes: Количество классов
+    :param to_categorical: Нужен ли smooth_label
+    '''
+    # Балансировка батча. Целое число (x,y) в кажом классе
+    bath_on_class = batch // num_classes
+    # Создание словаря с использованными индексами лэйблов
+    used_inxs = {key: [] for key in [inx for inx in range(num_classes)]}
+
+    ###############Доработать|||||||||
+    if batch != bath_on_class * num_classes:
+        # Если при batch / num_classes есть остаток от деления
+        # Узнать сколько не хватает
+        add_inxs = batch - bath_on_class * num_classes
+    ###############|||||||||||||||||||
+
+    while True:
+        with h5py.File(path, 'r') as f:
+            inxs = []  # Массив с рандомными индексами. Используются при формировании батча
+
+            ###############Доработать|||||||||
+            if batch != bath_on_class * num_classes:
+                rand_i = np.random.randint(f[y].shape[0], size=(add_inxs))
+            ###############|||||||||||||||||||
+
+            for class_ in range(num_classes):
+                # Фильтрация лэйблов по классам
+                # Если used_inxs не пустой, то избавляемся от использованных индексов
+                if len(used_inxs[class_]) > 0:
+                    # Создание маски по одному классу. Общая длинна y-вектора не меняется.
+                    mask_class = f[y][:] == class_ # -> bool
+                    # Присуждение False использованным индексам класса
+                    mask_class[used_inxs[class_]] = False
+                    # Получение индексов, где True
+                    mask_class = np.where(mask_class)[0]
+                    # Если при следующей итерации все индексы будут использованны,
+                    # то обнуляем список.
+                    if mask_class.shape[0] - bath_on_class < bath_on_class:
+                        used_inxs[class_] = []
+                else:
+                    mask_class = f[y][:] == class_
+                    mask_class = np.where(mask_class)[0]  # => тут индексы по классу[0] numpy
+                # Выбор случайных индексов, в количестве  bath_on_class
+                rand_inxs = np.random.choice(mask_class, bath_on_class)
+
+                # Добавление выбранных индексов от каждого класса в конец списка inxs
+                # и по ключу в словарь used_inxs
+                inxs.extend(rand_inxs)
+                used_inxs[class_] += rand_inxs  ################
+
+            ###############Доработать|||||||||
+            if batch != bath_on_class * num_classes:
+                # Если при batch / num_classes есть остаток от деления
+                # Узнать сколько не хватает
+                add_inxs = batch - bath_on_class * num_classes
+                # Выбираем рандомные индексы
+                rand_labels = np.random.randint(f[y].shape[0], size=(add_inxs))
+                # Добавляем рандомные индексы
+                inxs.append(rand_labels)
+            ###############|||||||||||||||||||
+
+            # Перевод в np.array,чтобы сделать .shuffle
+            inxs = np.array(inxs, dtype='int64')
+            np.random.shuffle(inxs)
+
+            # Собираем и скалируем x_batch
+            x_batch = [f[x][inx] for inx in inxs]
+            x_batch = np.array(x_batch, dtype=np.float32)
+            x_batch /= 128.
+            x_batch -= 1.
+
+            # Собираем y_bath
+            y_batch = [f[y][inx] for inx in inxs]
+            y_batch = np.array(y_batch, dtype=np.uint8)
+            if to_categorical:
+                # Если установлен флаг, то перевод в onehotencoding и размытие меток
+                y_batch = utils.to_categorical(y_batch, num_classes, dtype=np.uint8)
+                y_batch = new_smooth_label(y_batch)
+
+        yield x_batch, y_batch
+
